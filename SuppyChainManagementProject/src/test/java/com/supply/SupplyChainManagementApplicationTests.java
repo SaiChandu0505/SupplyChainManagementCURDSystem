08a26cec0a88a5666919c09fc98c268b51f2a6b0
SaiChandu55 <thisischandusai@gmail.com>
2023-01-13 11:38:11 +0530

    Initial Commit

---
+++ SuppyChainManagementProject/src/test/java/com/supply/SupplyChainManagementApplicationTests.java
@@ -0,0 +1,13 @@
+
+package com.supply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
